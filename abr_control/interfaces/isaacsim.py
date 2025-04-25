73d07bbc31b2d11141ec12e6e311585809f67370
Lea Steffen <lea.c.steffen@gmail.com>
2025-04-25 15:56:33 -0400

    implemented method connect in interface for isaacsim

--- abr_control/interfaces/isaacsim.py
+++ abr_control/interfaces/isaacsim.py
@@ -23,6 +23,11 @@ class IsaacSim(Interface):
         self.dt = dt  # time step
         self.count = 0  # keep track of how many times send forces is called
 
+        self.q = np.zeros(self.robot_config.N_JOINTS)  # joint angles
+        self.dq = np.zeros(self.robot_config.N_JOINTS)  # joint_velocities
+
+        #self.misc_handles = {}  # for tracking miscellaneous object handles
+
         '''
         self.q = np.zeros(self.robot_config.N_JOINTS)  # joint angles
         self.dq = np.zeros(self.robot_config.N_JOINTS)  # joint_velocities
@@ -34,11 +39,55 @@ class IsaacSim(Interface):
         '''
         
 
-
-
-    def connect(self):
+    def connect(self, load_scene=True):
         """All initial setup."""
         self.simulation_app = SimulationApp({"headless": False}) 
+        from isaacsim.core.api import World # type: ignore
+        from isaacsim.core.utils.extensions import get_extension_path_from_name # type: ignore
+        from isaacsim.asset.importer.urdf import _urdf # type: ignore
+        import omni.kit.commands # type: ignore
+        import omni.usd # type: ignore
+
+        self.robot_model = None
+        self.urdf_path = None
+        self.import_config = None
+        self.world = None
+        if load_scene:
+            # Create a world
+            self.world = World(physics_dt=self.dt,rendering_dt=self.dt)
+            self.world.scene.add_default_ground_plane()
+
+            # Acquire the URDF extension interface for parsing and importing URDF files
+            urdf_interface = _urdf.acquire_urdf_interface()
+
+            # Configure the settings for importing the URDF file
+            self.import_config = _urdf.ImportConfig()
+            self.import_config.convex_decomp = False  # Disable convex decomposition for simplicity
+            self.import_config.fix_base = True       # Fix the base of the robot to the ground
+            self.import_config.make_default_prim = True  # Make the robot the default prim in the scene
+            self.import_config.self_collision = False  # Disable self-collision for performance
+            self.import_config.distance_scale = 1     # Set distance scale for the robot
+            self.import_config.density = 0.0          # Set density to 0 (use default values)
+
+            # Retrieve the path of the URDF file from the extension
+            extension_path = get_extension_path_from_name("isaacsim.asset.importer.urdf")
+            root_path = extension_path + "/data/urdf/robots/ur10/urdf"
+            #root_path = extension_path + "/data/urdf/robots/ur5/urdf"
+            file_name = "ur10.urdf"
+            #file_name = "ur5.urdf"
+
+            # Parse the robot's URDF file to generate a robot model
+            result, self.robot_model = omni.kit.commands.execute(
+                "URDFParseFile",
+                urdf_path="{}/{}".format(root_path, file_name),
+                import_config=self.import_config
+                )
+
+        # Resetting the world needs to be called before querying anything related to an articulation specifically.
+        # Its recommended to always do a reset after adding your assets, for physics handles to be propagated properly
+        self.world.reset()
+
+
         print("Started Isaacsim as stand alone app...")
 
     def disconnect(self):
