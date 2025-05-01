657876005b9bb2fdaa9fe042f081e1dd4120ef9c
Lea Steffen <lea.c.steffen@gmail.com>
2025-05-01 11:48:06 -0400

    add method to provide pose

--- examples/isaacsim/flying_cube.py
+++ examples/isaacsim/flying_cube.py
@@ -48,3 +48,6 @@ class FlyingCube():
         position, orientation = self._cube.get_world_pose()
         linear_velocity = self._cube.get_linear_velocity()
         return np.concatenate([position, linear_velocity, orientation], axis=0)
+    
+    def _get_obj_pose(self):
+        return self._cube.get_world_pose()
